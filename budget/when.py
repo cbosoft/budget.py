2c81e8210da197bcb825fc16c6bdea618f979054
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-06-04 22:21:15 +0100

    Adds monthly option to when object

--- budget/when.py
+++ budget/when.py
@@ -2,25 +2,31 @@ from datetime import datetime
 
 class When:
 
-    def __init__(self, on=None, starts=None, ends=None):
+    def __init__(self, once_on=None, starts=None, ends=None, monthly_on=None):
 
-        assert on or starts
+        assert once_on or starts or monthly_on
 
-        if on:
-            assert isinstance(on, datetime)
+        if once_on:
+            assert isinstance(once_on, datetime)
         if starts:
-            assert isinstance(on, datetime)
+            assert isinstance(starts, datetime)
         if ends:
-            assert isinstance(on, datetime)
+            assert isinstance(ends, datetime)
+        if monthly_on:
+            assert isinstance(monthly_on, int)
 
-        self.on = on
+        self.once_on = once_on
         self.starts = starts
         self.ends = ends
+        self.monthly_on = monthly_on
 
     def matches(self, date):
         
-        if self.on:
-            return date == self.on
+        if self.once_on:
+            return date == self.once_on
+
+        if self.monthly_on:
+            return date.day == self.monthly_on # TODO: what if monthly_on is 31st and month is 30 days long? or what about Feb?
 
         if self.ends:
             return self.starts <= date <= self.ends
