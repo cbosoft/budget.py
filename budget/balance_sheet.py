ecef5a79e99e927d4026e6eeaa409272d1febd9c
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-06-05 16:38:03 +0100

    Adds scenario support

--- budget/balance_sheet.py
+++ budget/balance_sheet.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 
 from budget.item import Item
+from budget.scenario import Scenario
 
 def iter_dates(start, end):
     for i in range( (end-start).days ):
@@ -11,11 +12,25 @@ class BalanceSheet:
     def __init__(self, initial_value=0.0):
         self.initial_value = initial_value
         self.items = []
+        self.scenarios = {}
 
     def add_item(self, *args, **kwargs):
         self.items.append(Item(*args, **kwargs))
 
-    def plot(self, start=None, end=None):
+
+    def add_item_to_scenario(self, scenario, *args, **kwargs):
+        if scenario not in self.scenarios:
+            self.scenarios[scenario] = Scenario()
+        self.scenarios[scenario].items.append(Item(*args, **kwargs))
+
+
+    def plot_scenarios(self, *args, **kwargs):
+        self.plot(*args, **kwargs)
+        for scenario in self.scenarios.keys():
+            self.plot(*args, with_scenario=scenario, **kwargs)
+
+
+    def plot(self, start=None, end=None, with_scenario=False, month_every=3):
 
         today = datetime.today()
 
@@ -27,7 +42,15 @@ class BalanceSheet:
 
         balance = self.initial_value
         for date in iter_dates(start, end):
+            delta = 0.0
             for item in self.items:
-                # returns 0.0 if not a matching date
-                balance += item.check(date)
-        print(balance)
+                delta += item.check(date)
+
+            if with_scenario:
+                for item in self.scenarios[with_scenario].items:
+                    delta += item.check(date)
+
+            if balance:
+                balance.append(balance[-1] + delta)
+            else:
+                balance.append(self.initial_value + delta)
---
+++ budget/scenario.py
@@ -0,0 +1,6 @@
+class Scenario:
+
+    def __init__(self, items=None):
+        if not items:
+            items = list()
+        self.items = items
