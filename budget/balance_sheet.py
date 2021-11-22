25fd816190d7d7123c18528fe915c9c8029de17a
Christopher Boyle <chrismjboyle@gmail.com>
2021-11-22 19:26:35 +0000

    Get median monthly change, total balance at end, and merge all accounts onto a single plot (if desired)

--- budget/balance_sheet.py
+++ budget/balance_sheet.py
@@ -3,6 +3,7 @@ from datetime import date, timedelta
 import re
 
 from matplotlib import pyplot as plt
+import numpy as np
 
 from .items import Item
 from .scenario import Scenario
@@ -12,8 +13,9 @@ from .account import Account
 
 class BalanceSheet:
 
-    def __init__(self, name, *accounts: Account):
+    def __init__(self, name, *accounts: Account, single=False):
         self.name = name
+        self.single = single
         self.accounts = list(accounts)
         self.scenarios: Dict[str, Scenario] = {}
         self.start = date.today()
@@ -26,8 +28,9 @@ class BalanceSheet:
         if exc_type is None:
             self.plot()
 
-    def add_account(self, name: str, balance=0.0):
+    def add_account(self, name: str, balance=0.0) -> str:
         self.accounts.append(Account(name=name, current_value=balance))
+        return name
 
     def add_item(self, item, scenario='default'):
         if scenario not in self.scenarios:
@@ -44,14 +47,47 @@ class BalanceSheet:
     def plot(self):
         sim = FinanceSim()
         n = len(self.accounts)
-        fig, axes = plt.subplots(nrows=len(self.accounts), sharex='col', figsize=(7, 4*n))
-        for ax, acc in zip(axes, self.accounts):
-            plt.sca(ax)
-            plt.title(acc.name)
+        w, h = 7, 4
+        if self.single:
+            fig = plt.figure(figsize=(w, h))
+            ax = plt.gca()
+            axes = [ax]*n
+        else:
+            fig, axes = plt.subplots(nrows=len(self.accounts), sharex='col', figsize=(w, h*n))
+            for ax, acc in zip(axes, self.accounts):
+                plt.sca(ax)
+                plt.title(acc.name)
+
+        data = dict()
+        totals = 0
         for sce_n, scenario in self.scenarios.items():
             data = sim.run(scenario, self.start, self.end)
-            for i, (acc_n, v) in enumerate(data.items()):
+            for i, (acc_n, v) in enumerate(data['daily_data'].items()):
                 plt.sca(axes[i])
-                plt.plot(v, label=sce_n)
+                plt.plot(v, label=acc_n if self.single else sce_n)
+                totals = np.add(v, totals)
+            monthly_change = np.diff(data['monthly_data']['Current'])
+            median_change = np.median(monthly_change)
+            pm = '+' if median_change > 0 else '-'
+            change_str = f'{pm}£{median_change:.2f}'
+            if self.single:
+                plt.title(change_str)
+            else:
+                print(change_str)
+        # ax = plt.gca()
+        # plt.twinx()
+        # plt.plot(totals, color='k', label='total')
+        # plt.ylabel('Total £')
+        # locs, _ = plt.yticks()
+        # plt.yticks(locs, [f'{int(l//1000)}k' for l in locs])
+        # plt.sca(ax)
+        print(f'Total £{totals[-1]:.2f}')
+        month_ticks = data['month_ind']
+        plt.xticks(*zip(*month_ticks))
+        locs, _ = plt.yticks()
+        plt.yticks(locs, [f'{int(l//1000)}k' for l in locs])
+        plt.axhline(0, color='0.8')
+        plt.ylabel('£')
+        plt.legend(loc='upper left')
         plt.tight_layout()
         plt.show()
--- budget/sim.py
+++ budget/sim.py
@@ -1,5 +1,6 @@
 from typing import List, Dict
 from datetime import date, timedelta
+from calendar import monthrange, month_name
 
 from .scenario import Scenario
 
@@ -18,12 +19,31 @@ class FinanceSim:
             data[n].append(acc.value)
         self.t += self.delta_t
 
-        if end:
-            self.t = start_or_end
-        else:
-            end = start_or_end
-        data = {n: list() for n in scenario.accounts_by_name}
     def run(self, scenario: Scenario, start: date, end: date):
+        self.t = start
+        daily_data = {n: list() for n in scenario.accounts_by_name}
+        monthly_data = {n: list() for n in scenario.accounts_by_name}
+        step = 1
+        month_ind = []
         while self.t < end:
-            self.timestep(scenario, data)
-        return data
+            self.timestep(scenario, daily_data)
+            step += 1
+            if self.is_start_of_month():
+                for n, acc in scenario.accounts_by_name.items():
+                    monthly_data[n].append(acc.value)
+            if self.is_middle_of_month():
+                month_ind.append((step, month_name[self.t.month]))
+        return dict(
+            daily_data=daily_data,
+            monthly_data=monthly_data,
+            month_ind=month_ind)
+
+    def is_end_of_month(self) -> bool:
+        _, lom = monthrange(self.t.year, self.t.month)
+        return self.t.day == lom
+
+    def is_start_of_month(self) -> bool:
+        return self.t.day == 1
+
+    def is_middle_of_month(self) -> bool:
+        return self.t.day == 14
