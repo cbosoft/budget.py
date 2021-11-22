63507c384dcffef32c2cbaff29564216ea0e8b75
Christopher Boyle <chrismjboyle@gmail.com>
2021-11-22 19:19:21 +0000

    Add new 'When' objects to init

--- budget/__init__.py
+++ budget/__init__.py
@@ -1,6 +1,5 @@
-from budget.items import Income, Expense, Transfer
-from budget.when import When
-from budget.balance_sheet import BalanceSheet
-from budget.tax import after_tax
-from budget.plot import Plotter
+from .items import Income, Expense, Transfer
+from .when import Weekly, Monthly, Once, Daily
+from .balance_sheet import BalanceSheet
+from .tax import after_tax
 from .account import Account
